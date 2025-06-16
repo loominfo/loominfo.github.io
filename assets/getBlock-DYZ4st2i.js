1a641038ba051da746c62b64ec81336bf1c27a7a
loominfo <openloomofficial@gmail.com>
2025-06-16 18:29:59 +0800

    Update 202506161829

--- assets/getBlock-DYZ4st2i.js
+++
@@ -1,1 +0,0 @@
-import{b9 as a,ba as n}from"./index-BYI_izSd.js";async function i(i,t={}){const{chainId:c,...e}=t,o=i.getClient({chainId:c}),s=a(o,n,"getBlock");return{...await s(e),chainId:o.chain.id}}export{i as g};
---
+++ assets/isHash-N0HoaLYF.js
@@ -0,0 +1,1 @@
+import{b4 as r,b5 as i}from"./index-BEuE82eq.js";function n(n){return r(n)&&32===i(n)}export{n as i};
